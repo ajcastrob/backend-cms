82469e52346b84b5731a3df492c0b03d0c982d74
José Castro <a.j.castro.b@gmail.com>
2026-08-18 21:09:43 -0500

    ✨ [FEAT] BlogPage, ArticlePage, ArticleTag y repo_url
    Se crearon los modelos que se userán en la api.

--- config/urls.py
+++ config/urls.py
@@ -22,6 +22,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("", include("home.urls", namespace="home")),
     path("blog/", include("skills_blog.urls")),
 ]
 
--- skills_blog/models.py
+++ skills_blog/models.py
@@ -1,3 +1,76 @@
 from django.db import models
+from wagtail.models import Page
+from wagtail.fields import RichTextField
+from taggit.models import TaggedItemBase
+from wagtail.search import index
+from modelcluster.fields import ParentalKey
+from wagtail.admin.panels import FieldPanel
+from modelcluster.contrib.taggit import ClusterTaggableManager
+from datetime import date
+
 
 # Create your models here.
+class BlogPage(Page):
+    body = RichTextField(blank=True)
+
+    content_panels = Page.content_panels + [FieldPanel("body")]
+
+    template = "blog/blog_page.html"
+
+    def get_context(self, request):
+        tag = request.GET.get("tag")
+        if tag:
+            articles = (
+                ArticlePage.objects.filter(tags__name=tag)
+                .live()
+                .order_by("-first_published_at")
+            )
+        else:
+            articles = self.get_children().live().order_by("-first_published_at")
+
+        context = super().get_context(request)
+        context["articles"] = articles
+        context["tag"] = tag
+        return context
+
+
+class ArticlePage(Page):
+    intro = models.CharField(max_length=80)
+    body = RichTextField(blank=True)
+    date = models.DateField("Post date", default=date.today)
+    image = models.ForeignKey(
+        "wagtailimages.Image", on_delete=models.SET_NULL, null=True, related_name="+"
+    )
+    caption = models.CharField(blank=True, max_length=80)
+    tags = ClusterTaggableManager(through="ArticleTag", blank=True)
+    repo_url = models.URLField(blank=True)
+
+    def get_author(self):
+        return self.owner.username
+
+    def get_author_first_name(self):
+        return self.owner.first_name
+
+    search_fields = Page.search_fields + [
+        index.SearchField("intro"),
+        index.SearchField("get_author"),
+        index.SearchField("get_author_first_name"),
+    ]
+
+    content_panels = Page.content_panels + [
+        FieldPanel("intro"),
+        FieldPanel("image"),
+        FieldPanel("caption"),
+        FieldPanel("body"),
+        FieldPanel("date"),
+        FieldPanel("tags"),
+        FieldPanel("repo_url"),
+    ]
+
+
+class ArticleTag(TaggedItemBase):
+    content_object = ParentalKey(
+        ArticlePage,
+        on_delete=models.CASCADE,
+        related_name="tagged_items",
+    )
