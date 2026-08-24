6da57bd56ea4cd1ccc4effc73f81397ed8e0826f
José Castro <a.j.castro.b@gmail.com>
2026-08-24 02:54:36 -0500

    🔒️ [FIX] Se eleminó el correo personal de la api

--- skills_blog/models.py
+++ skills_blog/models.py
@@ -60,7 +60,6 @@ class OwnerProfile(Field):
             "username": value.username,
             "first_name": value.first_name,
             "last_name": value.last_name,
-            "email": value.email,
             "profile_picture": value.profile_picture.url
             if value.profile_picture
             else None,
